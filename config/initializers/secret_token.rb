d2a6a1c7a2e55a0f3eb153b6906d403348ab64e6
Bryce Foster <brycefoster@lbl.gov>
2012-05-09 16:12:29 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '70a3f786864bfea3d5646cec3622af7caccdfce82d213c92e6f8005056886399ddd9ee6df148b3457991e1fab35596f9078c74c6cffc9a2eb7c88629d5456c77'
